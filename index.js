433c982215d3359d1f289211c01607a428ee61b9
Barbara Washington <barbara.m.washington@gmail.com>
2019-01-24 23:05:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,8 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-  
+  const rankedList = document.querySelectorAll('.ranked-list li')
+    for (let i = 0; i < list.length; i++) {
+    list[i].innerHTML = (parseInt(list[i].innerHTML) + n).toString()
+  }
 }
